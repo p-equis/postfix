c8713f361465d67cd32493b7312abdc6507d9cc2
Paul Phillips <PaulPhillips@aggienetwork.com>
2013-09-19 17:13:07 -0500

    Fixed an issue where the rspec blew up on test-failures - I was deleting the current working directory. Oops.

--- cherry_pick_spec.rb
+++ cherry_pick_spec.rb
@@ -2,25 +2,27 @@ require "fileutils"
 
 describe "cherry-picking" do
 
-	before :all do
+	before :each do
 		@repository = GitRepository.new
 	end
 
-	after :all do
+	after :each do
 		@repository.destroy
 	end
 
-	it "should append text to the original message" do
+	xit "should append text to the original message" do
 		@repository.create_commit "first on trunk"
-		
 		@repository.create_branch "new_branch"
 		
 		@repository.create_commit "to cherry-pick"
+
+		@repository.cherry_pick ( :take=>'master', 
+			:to => 'new_branch', 
+			:merge_message => '[merged from trunk]' )
+
 		@repository.checkout_branch "new_branch"
-
-		# run_cherry_pick_script " +postfix"
-
-		#@repository.top_commit_message.should == "to cherry-pick +postfix"
+		
+		@repository.top_commit_message.should == "to cherry-pick[merged from trunk]"
 	end
 end
 
@@ -30,8 +32,7 @@ class GitRepository
 		@file_id = 1
 		destroy
 		Dir::mkdir @workspace
-		Dir::chdir @workspace
-		system("git init")
+		bash("git init")
 	end
 
 	def destroy
@@ -41,20 +42,28 @@ class GitRepository
 	def create_commit (message)
 		file_name = "#{@file_id}.txt"
 		@file_id += 1
-		system("touch #{file_name}")
-		system("git add #{file_name}")
-		system("git commit -m '#{message}'")
+		bash("touch #{file_name}")
+		bash("git add #{file_name}")
+		bash("git commit -m '#{message}'")
 	end
 
 	def create_branch (name)
-		system("git branch #{name}")
+		bash("git branch #{name}")
 	end
 
 	def checkout_branch (name)
-		system("git checkout #{name}")
+		bash("git checkout #{name}")
+	end
+
+	def cherry_pick (options)
+
 	end
 
 	def top_commit_message 
-		""#system("git log -1 --pretty=format:'%s'")
+		`git log -1 --pretty=format:'%s'`
+	end
+
+	def bash (command)
+		result = `(cd #{@workspace} && #{command})`
 	end
 end
