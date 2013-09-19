1e70275be13f65b45cfbce264cc10095d86032f9
Paul Phillips <PaulPhillips@aggienetwork.com>
2013-09-19 00:03:29 -0500

    Finished setup of multiple branches in the rspec

--- cherry_pick_spec.rb
+++ cherry_pick_spec.rb
@@ -1,4 +1,4 @@
-require 'fileutils'
+require "fileutils"
 
 describe "cherry-picking" do
 
@@ -11,13 +11,23 @@ describe "cherry-picking" do
 	end
 
 	it "should append text to the original message" do
+		@repository.create_commit "first on trunk"
 		
+		@repository.create_branch "new_branch"
+		
+		@repository.create_commit "to cherry-pick"
+		@repository.checkout_branch "new_branch"
+
+		# run_cherry_pick_script " +postfix"
+
+		#@repository.top_commit_message.should == "to cherry-pick +postfix"
 	end
 end
 
 class GitRepository
 	def initialize
 		@workspace = Dir::pwd + "/workspace"
+		@file_id = 1
 		destroy
 		Dir::mkdir @workspace
 		Dir::chdir @workspace
@@ -26,5 +36,25 @@ class GitRepository
 
 	def destroy
 		FileUtils.rm_rf @workspace
+	end
+
+	def create_commit (message)
+		file_name = "#{@file_id}.txt"
+		@file_id += 1
+		system("touch #{file_name}")
+		system("git add #{file_name}")
+		system("git commit -m '#{message}'")
+	end
+
+	def create_branch (name)
+		system("git branch #{name}")
+	end
+
+	def checkout_branch (name)
+		system("git checkout #{name}")
+	end
+
+	def top_commit_message 
+		""#system("git log -1 --pretty=format:'%s'")
 	end
 end
