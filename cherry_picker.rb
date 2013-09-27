191347282956ed23f6d600cd31c6f2ac3a167a29
Paul Phillips <PaulPhillips@aggienetwork.com>
2013-09-27 04:56:17 -0500

    Made the path to the shell script independent of the current working directory. It just has to be in the same folder as the cherry_picker

--- cherry_picker.rb
+++ cherry_picker.rb
@@ -9,10 +9,14 @@ class CherryPicker
 		merge_message = options[:merge_message]
 
 		checkout_branch target_branch
-		ENV["GIT_EDITOR"] = "sh #{Dir::pwd}/show.sh '#{merge_message}'"
+		ENV["GIT_EDITOR"] = "#{script} '#{merge_message}'"
 		run_in_subshell("git cherry-pick #{cherry} --edit")
 	end
 
+	def script
+		directory_of_this_file = File.expand_path(File.dirname(__FILE__))
+		"sh #{directory_of_this_file}/show.sh "
+	end
 
 	def checkout_branch (name)
 		run_in_subshell("git checkout #{name}")
