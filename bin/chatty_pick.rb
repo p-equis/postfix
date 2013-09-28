a16894e9024af8630c8ce8dea11a774e5dad4e7d
Paul Phillips <PaulPhillips@aggienetwork.com>
2013-09-28 02:01:17 -0500

    Simplified the bin/chatty_pick file

--- bin/chatty_pick.rb
+++ bin/chatty_pick.rb
@@ -1,15 +1,8 @@
 require 'cherry_picker'
 
-def cherry_pick_with_annotation(git_object_to_cherry_pick, target_branch)
-	puts git_object_to_cherry_pick, target_branch
+git_object_to_cherry_pick = ARGV[0]
+target_branch = ARGV[1]
 
-	CherryPicker.new(Dir.pwd).cherry_pick(:take => git_object_to_cherry_pick,
-		:to => target_branch,
-		:merge_message => "[merged from trunk]")
-end
-
-if __FILE__ == $0
-	git_object_to_cherry_pick = ARGV[0]
-	target_branch = ARGV[1]
-	cherry_pick_with_annotation(git_object_to_cherry_pick, target_branch)
-end
+CherryPicker.new(Dir.pwd).cherry_pick(:take => git_object_to_cherry_pick,
+	:to => target_branch,
+	:merge_message => "[merged from trunk]")
